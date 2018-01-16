6679ed922da06a864873bb9bd0f9cba714ecec17
ericaandrews <eureka.web.dev@gmail.com>
2018-01-16 12:29:00 -0800

    Create haiku.java

---
+++ haiku.java
@@ -0,0 +1,21 @@
+public class Haiku{
+  public static void main(String[] args)
+  {
+    //Haiku:
+    //Surf's Up! By Eureka
+    //Nervous excitement toward dangerous play
+    //I focus on my breathe
+    //This might be my last but not my first
+
+    //Escape sequence used: "\t" (horizontal tab) and "\r" (return)
+    System.out.println("Surf's Up! \t By: Erica Andrews \rSurf's Up");
+
+    //Print the first line of the Haiku using 2 print statements
+    System.out.print("Nervous excitement toward ");
+    System.out.println("dangerous play");
+
+    //Print the second and third lines of the Haiku using a single print statement 
+    //Escape sequence used: "\n" (Newline)
+    System.out.println("I focus on my breathe \nThis might be my last but not my first");
+  }
+} 
