4d35c1247e8e824cad7b7a95f16d28cd08a791b1
ericaandrews <eureka.web.dev@gmail.com>
2018-01-27 02:06:06 -0800

    Update and rename blackjack.java to blackJack.java

--- blackJack.java
+++ blackJack.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class BlackJack{
+public class blackJack{
   public static void main(String args[]){
     Scanner keyboard = new Scanner(System.in);
     Random random = new Random();
