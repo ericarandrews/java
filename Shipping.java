8e4dad2fa1dfa03c0754c2362a525d32db7eeee9
ericaandrews <eureka.web.dev@gmail.com>
2018-05-28 15:09:40 -0700

    Update Shipping.java

--- Shipping.java
+++ Shipping.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class Shipping {
-
-	public static void main(String[] args){
-
+public class Shipping 
+{
+	public static void main(String[] args)
+	{
+		
 		double weight_of_package = 0;
 		double distance = 0;
 
@@ -20,55 +21,71 @@ public class Shipping {
 		double rate_4 = 4.00;
 
 
-if(weight_of_package <= 2){
-	if(distance <= 500){
-		System.out.println("Shipping Charge: " + ((distance * rate_1) / 500));
-	}
-	else if(distance > 500 && distance <= 1000){
-		System.out.println("Shipping Charge: " + ((((distance - 500) * rate_1) / 500) + (rate_1)));
-	}
-	else if(distance > 1000 && distance <= 1500){
-		System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_1) / 500) + (rate_1 * 2)));
-	}
-}
+		if(weight_of_package <= 2)
+		{
+			if(distance <= 500)
+			{
+				System.out.println("Shipping Charge: " + ((distance * rate_1) / 500));
+			}
+			else if(distance > 500 && distance <= 1000)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 500) * rate_1) / 500) + (rate_1)));
+			}
+			else if(distance > 1000 && distance <= 1500)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_1) / 500) + (rate_1 * 2)));
+			}
+		}
 
 
-if(weight_of_package > 2 && weight_of_package <= 6){
-	if(distance <= 500){
-		System.out.println("Shipping Charge: " + ((distance * rate_2 ) / 500));
-	}
-	else if(distance > 500 && distance <= 1000){
-		System.out.println("Shipping Charge: " + ((((distance - 500) * rate_2 ) / 500) + (rate_2)));
-	}
-	else if(distance > 1000 && distance <= 1500){
-		System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_2) / 500) + (rate_2 * 2)));
-	}
-}
+		if(weight_of_package > 2 && weight_of_package <= 6)
+		{
+			if(distance <= 500)
+			{
+				System.out.println("Shipping Charge: " + ((distance * rate_2 ) / 500));
+			}
+			else if(distance > 500 && distance <= 1000)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 500) * rate_2 ) / 500) + (rate_2)));
+			}
+			else if(distance > 1000 && distance <= 1500)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_2) / 500) + (rate_2 * 2)));
+			}
+		}
 
 
-if(weight_of_package > 6 && weight_of_package <= 10){
-	if(distance <= 500){
-		System.out.println("Shipping Charge: " + ((distance * rate_3) / 500));
-	}
-	else if(distance > 500 && distance <= 1000){
-		System.out.println("Shipping Charge: " + ((((distance - 500) * rate_3) / 500) + (rate_3)));
-	}
-	else if(distance > 1000 && distance <= 1500){
-		System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_3) / 500) + (rate_3 * 2)));
-	}
-}
+		if(weight_of_package > 6 && weight_of_package <= 10)
+		{
+			if(distance <= 500)
+			{
+				System.out.println("Shipping Charge: " + ((distance * rate_3) / 500));
+			}
+			else if(distance > 500 && distance <= 1000)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 500) * rate_3) / 500) + (rate_3)));
+			}
+			else if(distance > 1000 && distance <= 1500)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_3) / 500) + (rate_3 * 2)));
+			}
+		}
 
 
-if(weight_of_package > 10){
-	if(distance <= 500){
-		System.out.println("Shipping Charge: " + (((distance * rate_4 ) / 500)));
-	}
-	else if(distance > 500 && distance <= 1000){
-		System.out.println("Shipping Charge: " + (((((distance - 500) * rate_4 ) / 500) + (rate_4))));
-	}
-	else if(distance > 1000 && distance <= 1500){
-		System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_4) / 500) + (rate_4 * 2)));
-	}
-}
-	}
+		if(weight_of_package > 10)
+		{
+			if(distance <= 500)
+			{
+				System.out.println("Shipping Charge: " + (((distance * rate_4 ) / 500)));
+			}
+			else if(distance > 500 && distance <= 1000)
+			{
+				System.out.println("Shipping Charge: " + (((((distance - 500) * rate_4 ) / 500) + (rate_4))));
+			}
+			else if(distance > 1000 && distance <= 1500)
+			{
+				System.out.println("Shipping Charge: " + ((((distance - 1000) * rate_4) / 500) + (rate_4 * 2)));
+			}
+		}
+   	 }
 }
