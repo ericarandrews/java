bee1f59148bf18c5b63b1a460ef9c35ede01d492
ericaandrews <eureka.web.dev@gmail.com>
2018-05-28 14:34:13 -0700

    Update NumberGuesser.java

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 
-public class NumberGuesser{
-  public static void main(String args[]){
+public class NumberGuesser
+{
+  public static void main(String args[])
+  {
     //Main Methods
-    do{
+    do
+    {
       playOneGame();
     }
     while(shouldPlayAgain());
@@ -11,7 +14,8 @@ public class NumberGuesser{
   
   //playOneGame(); method should have a return type of void
   //should implement a complete guessing game from 1 to 100.
-    public static void playOneGame(){
+    public static void playOneGame()
+    {
       System.out.println("\nWelcome to the Number Guess Game. Think of a number between 1 and 100...\n");
       int low = 0;
       int high = 101;
@@ -19,18 +23,22 @@ public class NumberGuesser{
       int usersResponseToGuess;
       
       //Invoke function getUserResponseToGuess(); and getMidPoint(); inside playOneGame();
-      do{
+      do
+      {
         guess = getMidPoint(low, high);
         usersResponseToGuess = getUsersResponseToGuess(guess);
         //users guess is placed within method getUsersResponseToGuess()
         
-        if(usersResponseToGuess == 'h'){
+        if(usersResponseToGuess == 'h')
+        {
           low = guess;
         }
-        else if(usersResponseToGuess == 'l'){
+        else if(usersResponseToGuess == 'l')
+        {
           high = guess;
         }
-        else if(usersResponseToGuess == 'c'){
+        else if(usersResponseToGuess == 'c')
+        {
         }
       }
       while(usersResponseToGuess != 'c');
@@ -41,12 +49,14 @@ public class NumberGuesser{
     //prompt the user to determine if the user wants to play again
     //read in a character; return true if the character is 'y'
     //otherwise, return false
-    public static boolean shouldPlayAgain(){
+    public static boolean shouldPlayAgain()
+    {
       System.out.print("Good Game! Enter yes or no to play again: ");
       Scanner keyboard = new Scanner(System.in);
       char playAgain = keyboard.next().charAt(0);
       
-      if(playAgain == 'y'){
+      if(playAgain == 'y')
+      {
         return true;
       }
       else return false;
@@ -56,11 +66,13 @@ public class NumberGuesser{
     //user inputs value replacing the token <guess>
     //return char; char should be one of the three: 'h', 'l', or 'c'
     //Must have the following method title: public static char getUsersResponseToGuess(int guess){
-    public static char getUsersResponseToGuess(int guess){
+    public static char getUsersResponseToGuess(int guess)
+    {
       Scanner keyboard = new Scanner(System.in);
       char inputedValue; //user inputs whether guess is higher, lower or correct
       
-      do{
+      do
+      {
         System.out.print("Is it " + guess + "? Enter 'h' for higher, 'l' for lower, or 'c' for correct: ");
         inputedValue = keyboard.next().charAt(0);
       }
@@ -72,7 +84,8 @@ public class NumberGuesser{
     //getMidPoint(); function should except two integers; should return the mid point of the two integers
     //if two values in the middle of range, chose smaller of the two
     //Must have the following method title: public static int getMidPoint(int low, int high){
-    public static int getMidPoint(int low, int high){
+    public static int getMidPoint(int low, int high)
+    {
       int mid;
       mid = (high + low)/2;
       return mid;
