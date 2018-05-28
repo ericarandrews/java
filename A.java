e569fc0971c70238c5a0d7f6604b62bccfeaedc4
ericaandrews <eureka.web.dev@gmail.com>
2018-05-28 14:55:20 -0700

    Update A.java

--- A.java
+++ A.java
@@ -15,17 +15,20 @@ public class A
 	System.out.print("Enter the number of attendees in the meeting: ");
 	num_of_attendees = keyboard.nextInt();
 
-	if(num_of_attendees < room_capacity){
+	if(num_of_attendees < room_capacity)
+	{
 		System.out.print("\nThe meeting is legally not in violation of fire laws. ");
 		System.out.println("\nThe number of additional people that may be added = " + (room_capacity - num_of_attendees));
 	}
-	else if(num_of_attendees == room_capacity){
+	else if(num_of_attendees == room_capacity)
+	{
 		System.out.print("\nThe meeting is legally not in violation of fire laws. ");
 		System.out.println("\nThe number of attendees equals the rooms capacity.");
 	}
-	else{
+	else
+	{
 		System.out.print("\nThe meeting is legally in violation of fire laws. ");
 		System.out.println("\nThe number of excess people that need to be removed = " + (num_of_attendees - room_capacity));
-	};
-   }
+	}
+    }
 }
