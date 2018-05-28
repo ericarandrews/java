32dd96092c55dfe453ba011dd8a17eb0940e7b82
ericaandrews <eureka.web.dev@gmail.com>
2018-05-28 14:54:26 -0700

    Update Average.java

--- Average.java
+++ Average.java
@@ -10,15 +10,18 @@ public class Average
 		System.out.println("Enter number of values: ");
 		values = keyboard.nextInt();
 		
-		if(values == 0) {
+		if(values == 0) 
+		{
 			System.out.println("Average: " + values);
 		}
 		
 		double total = 0;
 		int x = 0;
 	
-		while(values != 0 && x < values){
-			for(x = 1; x < values + 1; x++){
+		while(values != 0 && x < values)
+		{
+			for(x = 1; x < values + 1; x++)
+			{
 				double value_total;
 				System.out.println("Enter value total for value #" + x + ": ");
 				value_total = keyboard.nextDouble();
