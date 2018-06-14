d8df35708dee7bae9c7e02869dd65f5654420b67
ericaandrews <eureka.web.dev@gmail.com>
2018-06-14 00:08:14 -0700

    Create GridItem.java

---
+++ GridItem.java
@@ -0,0 +1,10 @@
+public interface GridItem {
+		
+	public int getX();
+	public void setX(int value);
+	public int getY();
+	public void setY(int value);
+	public abstract double getArea();
+	public abstract boolean containsPoint(int xValue, int yValue);
+
+}
