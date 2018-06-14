bafc1fc4a6ff6a384e267dca7278ed70dffc0933
ericaandrews <eureka.web.dev@gmail.com>
2018-06-14 02:18:24 -0700

    Create AddingExceptions_MyRectangle_GridWriter.java

---
+++ AddingExceptions_MyRectangle_GridWriter.java
@@ -0,0 +1,40 @@
+public class AddingExceptions_MyRectangle_GridWriter implements GridItem 
+{
+	private int height;
+	private int width;
+	
+	protected int x;
+	protected int y;
+	
+	public AddingExceptions_MyRectangle_GridWriter(int xValue, int yValue, int w, int h) {
+		x = xValue;
+		y = yValue;
+		width = w;
+		height = h;
+	}
+	
+	@Override
+	public int getX() {return x;}
+	
+	@Override
+	public void setX(int value) {x = value;}
+	
+	@Override
+	public int getY() {return y;}
+	
+	@Override
+	public void setY(int value) {y = value;}
+
+	@Override
+	public double getArea() {
+			return height * width;
+	}
+	
+	@Override
+	public boolean containsPoint(int xValue, int yValue) {
+			return	xValue >= x &&
+			xValue <= x + width &&
+			yValue >= y &&
+			yValue <= y + height;
+	}
+}
