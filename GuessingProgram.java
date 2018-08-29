46a187b2c3e448386b17665cafedd8ac9066a734
ericaandrews <eureka.web.dev@gmail.com>
2018-08-28 23:15:36 -0700

    Create GuessingProgram.java

---
+++ GuessingProgram.java
@@ -0,0 +1,80 @@
+// The player is asked to guess a number and whether 
+// the number guessed by the program is the player’s guess. 
+// The player is asked to respond to the programs guess by 
+// stating whether the guess is correct, a higher value, or 
+// a lower value. This project was completed in Java.
+// NumberGuessingGame.java
+
+import java.util.Scanner;
+
+public class GuessingProgram
+{
+	public static void main(String[] args) 
+	{		
+		do 
+		{
+			playGame();
+		}
+		
+		while (shouldPlayAgain());	
+	}	
+
+			public static void playGame()
+			{	
+				System.out.println("\nWelcome to the Number Guessing Game. Think of a number between 1 and 100... \n");
+				int lowerBound = 0;
+				int upperBound = 101;
+				char guess;
+				NumberGuessingGame guesser = new NumberGuessingGame(lowerBound, upperBound);
+
+				do
+				{
+					// users guess placed within method/function guesser.getCurrentGuess();
+					System.out.println("Is it " + guesser.getCurrentGuess() + "?"); 
+					System.out.print("Enter 'h' for higher, 'l' for lower, 'c' for correct, or 'r' to reset: ");	
+					Scanner input = new Scanner(System.in);
+					guess = input.next().charAt(0);
+								
+					//void higher method
+					if(guess == 'h')
+					{
+						guesser.higher();
+					} 
+
+					//void lower method
+					else if(guess == 'l')
+					{
+						guesser.lower();
+					}
+					
+					//getCurrentGuess method
+					else if(guess == 'c')
+					{
+						System.out.println("You picked " + guesser.getCurrentGuess() + "!");
+						System.out.println("Good Game!");
+							
+					}
+					
+					//void reset method
+					else if(guess == 'r')
+					{
+						guesser.reset();
+					}
+				}
+				while(guess != 'c'); 
+		}	
+
+			public static boolean shouldPlayAgain()
+			{
+				System.out.print("\nEnter yes or no to play again: ");
+				Scanner input = new Scanner(System.in);
+				char playAgain = input.next().charAt(0);
+			
+				if (playAgain == 'y')
+				{
+				return true;
+				}
+				else return false;	
+			}
+}
+	
