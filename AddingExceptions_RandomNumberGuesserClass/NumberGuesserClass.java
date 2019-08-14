bc48ca9f64ecbeae1c38420002d9f27d7380702a
ericarandrews <eureka.web.dev@gmail.com>
2019-08-14 16:57:03 -0700

    Add files via upload

---
+++ AddingExceptions_RandomNumberGuesserClass/NumberGuesserClass.java
@@ -0,0 +1,31 @@
+
+public class NumberGuesserClass {
+	protected int high;
+	protected int low;
+	
+	protected int lower;
+	protected int higher;
+	
+	public NumberGuesserClass(int lowerBound, int upperBound) 
+	{
+		low = lower = lowerBound;
+		high = higher = upperBound;
+	}
+	public int getCurrentGuess() 
+	{
+		return (high + low) / 2;
+	}
+	public void higher() 
+	{
+		low = getCurrentGuess() + 1;
+	}
+	public void lower() 
+	{
+		high = getCurrentGuess() - 1;
+	}
+	public void reset() 
+	{
+		low = lower;
+		high = higher;
+	}
+}
