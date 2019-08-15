ec0817b1d5e77db38214ee544c9ef6543d9dba3f
ericarandrews <eureka.web.dev@gmail.com>
2019-08-14 17:02:33 -0700

    Add files via upload

---
+++ AddingExceptions/AddingExceptions_GridWriter/GridItem.java
@@ -0,0 +1,11 @@
+	
+public interface GridItem {
+		
+	public int getX();
+	public void setX(int value);
+	public int getY();
+	public void setY(int value);
+	public abstract double getArea();
+	public abstract boolean containsPoint(int xValue, int yValue);
+
+}
